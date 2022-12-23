23d799c5597471a475b21ef89aa4a04f5300081c
fojanb <fozhanmail@gmail.com>
2022-12-23 13:46:32 -0800

    init

---
+++ Day-3-Theme-Toggler/app.js
@@ -0,0 +1,7 @@
+const data = {
+  modes: [document.querySelector("#moon"), document.querySelector("#sun")],
+  toggler: () => {
+    document.body.classList.toggle("sun");
+  },
+};
+data.modes.forEach((mode) => mode.addEventListener("click", data.toggler));
