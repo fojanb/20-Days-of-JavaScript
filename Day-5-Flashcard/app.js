9f4cd9ff2a81edf1d9809138757c640ee9353b0b
fojanb <fozhanmail@gmail.com>
2022-12-25 13:56:53 -0800

    Setting local storage

--- Day-5-Flashcard/app.js
+++ Day-5-Flashcard/app.js
@@ -3,52 +3,77 @@ const data = {
   newCard: document.createElement("form"),
   question: "",
   answer: "",
-  save: (e, q, a, question, answer) => {
-    e.preventDefault();
+  questions: [],
+};
+const saveToLocalStorage = () => {
+  localStorage.setItem("data", JSON.stringify(data.questions));
+};
+const createFlashCard = (questions) => {
+  questions.forEach(question => {
     const flashCard = document.createElement("div");
-    if (q === "" || a === "") {
-      flashCard.classList.add("hide");
-      alert("Please provide both answer and question.");
-      return;
-    }
     flashCard.classList.add("flashCard");
-    flashCard.innerHTML = `<p>${q}</p><p id="showHide" style="color:blue;cursor:pointer;
-    ">Show/Hide Answer</p>`;
+    flashCard.innerHTML = `
+      <p>${question.Q}</p><p class="showHide" style="color:blue;cursor:pointer;">Show/Hide Answer</p>`;
     document.querySelector(".flashCards").append(flashCard);
     document
-      .querySelector("#showHide")
-      .addEventListener("click", (e) => e.target.innerHTML = `<p style="color:whitesmoke;">${a}</p>`);
+      .querySelector(".showHide")
+      .addEventListener(
+        "click",
+        (e) => (e.target.innerHTML = `<p style="color:whitesmoke;">${question.A}</p>`)
+      );
+  });
+
+};
+const saveBtn = (e, q, a, question, answer) => {
+  e.preventDefault();
+  data.questions.push({ Q: q, A: a });
+  if (q === "" || a === "") {
+    alert("Please provide both answer and question.");
+  } else {
+    saveToLocalStorage();
+    createFlashCard(data.questions);
+    // ---------------------
     question.value = "";
     answer.value = "";
-    data.question = "";
-    data.answer = "";
-  },
-  close: (e) => {
-    e.preventDefault();
-    e.target.parentNode.classList.add("hide");
-  },
+  }
+};
+const closeBtn = (e) => {
+  e.preventDefault();
+  e.target.parentNode.classList.add("hide");
 };
 const makeCard = () => {
   data.newCard.classList.remove("hide");
   data.newCard.classList.add("form");
   data.newCard.innerHTML = `
-      <span id="close">&times;</span>
-      <lable for="question">Question</lable>
-      <textarea id="question" style="height:100px;width: 90%;" value=""></textarea>
-      <lable for="answer">Answer</lable>
-      <textarea id="answer" style="height:100px;width: 90%;" value=""></textarea>
-      <button id="save" style="width:30%;">Save</button>
-        `;
+    <span id="close">&times;</span>
+    <lable for="question">Question</lable>
+    <textarea id="question" style="height:100px;width: 90%;" value=""></textarea>
+    <lable for="answer">Answer</lable>
+    <textarea id="answer" style="height:100px;width: 90%;" value=""></textarea>
+    <button id="save" style="width:30%;">Save</button>
+      `;
   document.querySelector(".container").append(data.newCard);
   const close = document.querySelector("#close");
   const question = document.querySelector("#question");
   const answer = document.querySelector("#answer");
   const save = document.querySelector("#save");
-  close.addEventListener("click", (e) => data.close(e));
+  close.addEventListener("click", (e) => closeBtn(e));
   question.addEventListener("change", (e) => (data.question = e.target.value));
   answer.addEventListener("change", (e) => (data.answer = e.target.value));
   save.addEventListener("click", (e) =>
-    data.save(e, data.question, data.answer, question, answer)
+    saveBtn(e, data.question, data.answer, question, answer)
   );
 };
-data.add.addEventListener("click", makeCard);
+const fetchQuestions = () => {
+  let currentQuestions = JSON.parse(localStorage.getItem("data"));
+  if (!currentQuestions) {
+    return;
+  }
+  data.questions.push(...currentQuestions);
+  createFlashCard(data.questions)
+  // console.log(data.questions);
+};
+window.addEventListener("load", () => {
+  data.add.addEventListener("click", makeCard);
+  fetchQuestions();
+});
