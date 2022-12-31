39d20a1e3b631465fb83220b4082f02fcd0be69c
fojanb <fozhanmail@gmail.com>
2022-12-30 19:43:16 -0800

    Almost done

---
+++ Day-7-Addition-Game/app.js
@@ -0,0 +1,29 @@
+const data = {
+  num1: document.querySelector(".num1"),
+  num2: document.querySelector(".num2"),
+  result: document.querySelector(".result"),
+  btn: document.querySelector(".btn"),
+  res: 0,
+};
+const randomNum = () => {
+  data.num1.innerText = Math.ceil(Math.random() * 10);
+  data.num2.innerText = Math.ceil(Math.random() * 10);
+};
+const buttonHandler = () => {
+  data.btn.addEventListener("click", () => {
+    if (data.res == data.result.value) {
+      alert(`Your anwer is correct : ${data.result.value}`);
+    } else {
+      alert(`Sorry, incorrect. The correct answer was : ${data.res}`);
+    }
+  });
+};
+const checkResult = (n1, n2) => {
+  data.res = parseInt(n1.innerText) + parseInt(n2.innerText);
+};
+
+window.addEventListener("load", () => {
+  randomNum();
+  checkResult(data.num1, data.num2);
+  buttonHandler();
+});
