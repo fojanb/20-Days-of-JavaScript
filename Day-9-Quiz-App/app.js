e20353fe058cdcf2b9a5d9761250571c1aa1b0fc
fojanb <fozhanmail@gmail.com>
2023-01-01 15:07:24 -0800

    Rendering data through HTML

--- Day-9-Quiz-App/app.js
+++ Day-9-Quiz-App/app.js
@@ -1,10 +1,26 @@
 const data = {
-  question: document.querySelector(".question"),
   questions: [],
+  container: document.querySelector(".container"),
 };
 const fetchQuestions = async () => {
   const response = await fetch("./questions.json");
   data.questions = await response.json();
-};
+  // console.log(data.questions.questions);
+  data.questions.questions.forEach((question) => {
+    const card = document.createElement("div");
+    card.setAttribute("id", question.id);
+    card.innerHTML = `
+        <div class="question">${question.question}</div>
+        <ol>
+            <li>${question.opt_1}</li>
+            <li>${question.opt_2}</li>
+            <li>${question.opt_3}</li>
+            <li>${question.opt_4}</li>
+        </ol>
+        <button id="btn-${question.id}">Next Question</button>
 
-window.addEventListener("load", fetchQuestions());
+        `;
+    data.container.append(card);
+  });
+};
+window.addEventListener("load", fetchQuestions);
