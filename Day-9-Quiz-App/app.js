f3f720689a185a778bbe03190f602e357401081d
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-01-03 13:51:02 -0800

    Render page with new question from json file

--- Day-9-Quiz-App/app.js
+++ Day-9-Quiz-App/app.js
@@ -2,6 +2,12 @@ const data = {
   questions: [],
   container: document.querySelector(".container"),
 };
+const nextBtn = (id) => {
+  document.getElementById(`btn-${id}`).parentNode.classList.add("hidden");
+  document
+    .getElementById(`btn-${id}`)
+    .parentNode.nextSibling.classList.remove("hidden");
+};
 const fetchQuestions = async () => {
   const response = await fetch("./questions.json");
   data.questions = await response.json();
@@ -9,17 +15,30 @@ const fetchQuestions = async () => {
   data.questions.questions.forEach((question) => {
     const card = document.createElement("div");
     card.setAttribute("id", question.id);
-    card.innerHTML = `
-        <div class="question">${question.question}</div>
-        <ol>
-            <li>${question.opt_1}</li>
-            <li>${question.opt_2}</li>
-            <li>${question.opt_3}</li>
-            <li>${question.opt_4}</li>
-        </ol>
-        <button id="btn-${question.id}">Next Question</button>
-        `;
-    data.container.append(card);
+    const showQuestion = (hide) => {
+      card.classList.add("question", hide);
+      card.innerHTML = `
+      <div class="question-title">${question.question}</div>
+      <div class="question-options">
+          <div><input type="checkbox"></input>${question.opt_1}</div>
+          <div><input type="checkbox"></input>${question.opt_2}</div>
+          <div><input type="checkbox"></input>${question.opt_3}</div>
+          <div><input type="checkbox"></input>${question.opt_4}</div>
+      </div>
+      <button id="btn-${question.id}">Next Question</button>
+      `;
+      data.container.append(card);
+      document
+        .getElementById(`btn-${question.id}`)
+        .addEventListener("click", () => nextBtn(question.id));
+    };
+
+    if (question.id == 0) {
+      showQuestion(null);
+    } else {
+      showQuestion("hidden");
+    }
   });
 };
+
 window.addEventListener("load", fetchQuestions);
