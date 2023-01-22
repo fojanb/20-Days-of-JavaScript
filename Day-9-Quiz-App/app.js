aeeed093a1e4ba25c7ed9c47cbb2e580a78585d8
fojanb <fozhanmail@gmail.com>
2023-01-21 22:39:40 -0800

    Add functions to manage states

--- Day-9-Quiz-App/app.js
+++ Day-9-Quiz-App/app.js
@@ -1,38 +1,62 @@
-const data = {
-  questions: [],
-  container: document.querySelector(".container"),
-};
-const generateCard = (questions) => {
-  questions.forEach((question) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.style.display = `${question.show}`
-    card.setAttribute("id", question.id);
-    card.innerHTML = `
-        <div class="question">${question.question}</div>
-        <ol>
-            <li><input type="checkbox"/>${question.opt_1}</li>
-            <li><input type="checkbox"/>${question.opt_2}</li>
-            <li><input type="checkbox"/>${question.opt_3}</li>
-            <li><input type="checkbox"/>${question.opt_4}</li>
-        </ol>
-        <button id="btn-${question.id}">Next Question</button>
-        `;
-    if(question.active){
-      data.container.append(card);
-    }
-      
-    
-    document.getElementById(`btn-${question.id}`).addEventListener("click",() =>{
-      document.getElementById(`${question.id}`).style.display = "none";
-    });
-  });
-};
-const fetchQuestions = async () => {
-  const response = await fetch(`./questions.json`);
-  data.questions = await response.json();
-  // console.log(data.questions.questions);
-  generateCard(data.questions.questions);
-};
+// const data = {
+//   questions: [],
+//   container: document.querySelector(".container"),
+//   endPoint:"./questions.json",
+// };
+// const generateCard = (questions) => {
+//   questions.forEach((question) => {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+//     card.setAttribute("id", question.id);
+//     card.innerHTML = `
+//         <div class="question">${question.question}</div>
+//         <ol>
+//             <li><input type="checkbox"/>${question.opt_1}</li>
+//             <li><input type="checkbox"/>${question.opt_2}</li>
+//             <li><input type="checkbox"/>${question.opt_3}</li>
+//             <li><input type="checkbox"/>${question.opt_4}</li>
+//         </ol>
+//         <button id="btn-${question.id}">Next Question</button>
+//         `;
+//       data.container.append(card);
+//   });
+// };
+// const fetchQuestions = async () => {
+//   const response = await fetch(data.endPoint);
+//   data.questions = await response.json();
+//   console.log(data.questions.questions);
+//   generateCard(data.questions.questions);
+// };
 
-window.addEventListener("load", fetchQuestions);
+// window.addEventListener("load", fetchQuestions);
+// ---------------No API and No json file----------------------------
+
+const data = {
+  current : "",
+  prev:"",
+  next:"",
+  slides : document.querySelector(".slides"),
+  prevBtn : document.querySelector(".goToPrev"),
+  nextBtn : document.querySelector(".goToNext"),
+
+}
+
+const startSlider = () => {
+  data.current = data.slides.querySelector(".current");
+  data.prev = data.current.previousElementSibling || data.slides.lastElementChild;;
+  data.next = data.current.nextElementSibling || data.slides.firstElementChild;;
+  console.log({
+    current: data.current,
+    prev : data.prev,
+    next : data.next,
+  })
+
+}
+const applyClasses = () => {
+  data.current.classList.add("current");
+  data.prev.classList.add("prev");
+  data.next.classList.add("next");
+
+}
+startSlider();
+applyClasses();
