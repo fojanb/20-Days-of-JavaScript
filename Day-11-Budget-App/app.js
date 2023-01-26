f0aad2c7b3597059cc1ed9759686a37a7b50e67b
fojanb <fozhanmail@gmail.com>
2023-01-25 18:16:09 -0800

    Adding js

--- Day-11-Budget-App/app.js
+++ Day-11-Budget-App/app.js
@@ -1,12 +1,10 @@
 const data = {
     budgetForm : document.querySelector("#budget-form"),
-
+    current_budget: document.getElementById("budget"),
+    budget : 0,
 }
-data.budgetForm.querySelector("input").addEventListener("change",(e) => {
-    console.log(e)
-});
 data.budgetForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(e.currentTarget.value)
-
+    data.budget = parseInt(data.budgetForm.querySelector("input").value);
+    data.current_budget.innerText = data.budget;
 })
