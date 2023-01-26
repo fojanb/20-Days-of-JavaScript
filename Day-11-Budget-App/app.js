39d6aeb9978d1f94619f47433645aeeaeacfcb9d
fojanb <fozhanmail@gmail.com>
2023-01-25 19:33:17 -0800

    Adding js

--- Day-11-Budget-App/app.js
+++ Day-11-Budget-App/app.js
@@ -1,10 +1,22 @@
 const data = {
-    budgetForm : document.querySelector("#budget-form"),
-    current_budget: document.getElementById("budget"),
-    budget : 0,
-}
+  budgetForm: document.querySelector("#budget-form"),
+  current_budget: document.getElementById("budget"),
+  expenseForm: document.getElementById("expense-form"),
+  current_expense: document.getElementById("expense"),
+  expense_title: document.getElementById("expense_title"),
+  
+};
 data.budgetForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    data.budget = parseInt(data.budgetForm.querySelector("input").value);
-    data.current_budget.innerText = data.budget;
-})
+  e.preventDefault();
+  data.current_budget.innerText = parseInt(data.budgetForm.querySelector("input").value);
+  data.budgetForm.querySelector("input").value = "";
+});
+data.expenseForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  data.current_expense.innerText = parseInt(
+    data.expenseForm.querySelector("#expenseAmount").value
+  );
+  data.expense_title.innerText = `-${document.getElementById("title").value}`;
+  document.getElementById("title").value="";
+  data.expenseForm.querySelector("#expenseAmount").value="";
+});
