ae2143e7fb9107ec3cae2448cac48a8125903751
fojanb <fozhanmail@gmail.com>
2023-03-12 15:45:47 -0700

    adding custome module

--- Day-2-Tip-Calculator/app.js
+++ Day-2-Tip-Calculator/app.js
@@ -1,13 +1,4 @@
-const data = {
-  billInput: document.querySelector("#billAmount"),
-  personsInput: document.querySelector("#persons"),
-  dropDownInput: document.querySelector("select"),
-  btn: document.querySelector("button"),
-  reset: document.querySelector("#reset"),
-  bill: 0,
-  persons: 0,
-  tip: 0,
-};
+import {data} from "./utils.js";
 data.personsInput.addEventListener("focus", (e) => {
   e.target.parentNode.querySelector("i").style.display = "none";
 });
