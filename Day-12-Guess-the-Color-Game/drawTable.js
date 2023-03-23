5308054833b14189c26e1243f9787a7a3f1aa826
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-03-23 16:30:24 -0700

    adding function

--- Day-12-Guess-the-Color-Game/drawTable.js
+++ Day-12-Guess-the-Color-Game/drawTable.js
@@ -1,13 +1,17 @@
 import { generateColor } from "./targetColor.js";
 import { table } from "./app.js";
+import { targetColor } from "./targetColor.js";
+let randomIndex = Math.floor((Math.random() * 2));
 const drawTable = () => {
   for (let index = 0; index < 3; index++) {
     let row = document.createElement("tr");
+    row.setAttribute("id",`row-${index}`)
     row.innerHTML = `
         <td><div style="background-color:${generateColor()};width:50px;height:50px;border-radius:50%;cursor:pointer;"></div></td>
         <td><div style="background-color:${generateColor()};width:50px;height:50px;border-radius:50%;cursor:pointer;"></div></td>
         <td><div style="background-color:${generateColor()};width:50px;height:50px;border-radius:50%;cursor:pointer;"></div></td>`;
-    table.append(row);
+        table.append(row);
+        
   }
   document.querySelectorAll("td div").forEach((color) => {
     color.addEventListener("click", (e) => {
