73f0b87377f1f1c0cb54e66c33e2c3c927296dbc
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-03-28 13:35:40 -0700

    (^_^)

---
+++ Day-13-Modal/app.js
@@ -0,0 +1,13 @@
+const openModal = document.getElementById("openModal");
+const modal = document.querySelector(".modal");
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+  document.querySelector(".closeModal").addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
