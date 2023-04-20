1d1be716a5fcb9fa7459b7d499e818f2da9d813b
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-04-20 15:35:19 -0700

    Make an API call for the get request and store data in a variable

--- Day-16-Search-Engine/app.js
+++ Day-16-Search-Engine/app.js
@@ -1,5 +1,4 @@
 import { API_KEY_1, API_KEY_2 } from "./myapikeys.js";
-<<<<<<< HEAD
 const searchInput = document.getElementById("search");
 const searchButton = document.querySelector("button");
 const options = {
@@ -27,28 +26,5 @@ const getImage = async () => {
 };
 window.addEventListener("load", () => {
   searchButton.addEventListener("click", getImage);
-=======
-const search_bar = document.getElementById("search");
-let card = document.createElement("div");
-card.classList.add("card");
-search_bar.addEventListener("change", async (e) => {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY_1,
-      "X-RapidAPI-Host": API_KEY_2,
-    },
-  };
 
-  let data = await fetch(
-    `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=${e.target.value}&pageNumber=1&pageSize=10&autoCorrect=true`,
-    options
-  ).then((response) => response.json());
-  data.relatedSearch.forEach(item => {
-    let place = document.createElement("div");
-    place.innerHTML = item;
-    document.body.append(place)
-  })
-  console.log(data);
->>>>>>> a78c9b31f49fd139eb0588daebeeb70b273ae33d
 });
