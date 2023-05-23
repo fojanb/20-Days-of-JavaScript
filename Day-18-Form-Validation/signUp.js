f3ec50d7dc0f52c469d4f5ecc2d3f6553db77917
fojanb <fozhanmail@gmail.com>
2023-05-22 22:01:18 -0700

    Add a checkbox to toggle between hide password and show password

--- Day-18-Form-Validation/signUp.js
+++ Day-18-Form-Validation/signUp.js
@@ -5,11 +5,12 @@ const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const mobileNumber = document.getElementById("mobileNumber");
 const form = document.getElementById("valid_form");
+const show_passwrod = document.getElementById("visibility");
 const input_validation = (user_input, input_alert, e) => {
   if (!user_input.value) {
     e.preventDefault();
     user_input.nextElementSibling.classList.add("text-danger");
-    user_input.nextElementSibling.innerText = `**Please fill the ${input_alert} field.`;
+    user_input.nextElementSibling.innerText = `** Please fill the ${input_alert} field.`;
   } else {
     return;
   }
@@ -18,44 +19,69 @@ const name_validation = (name_input, msg, e) => {
   if (!name_input.value) {
     e.preventDefault();
     name_input.nextElementSibling.classList.add("text-danger");
-    name_input.nextElementSibling.innerText = `**Please fill the ${msg} field.`;
+    name_input.nextElementSibling.innerText = `** Please fill the ${msg} field.`;
     return;
   }
   if (!isNaN(name_input.value)) {
     e.preventDefault();
     name_input.nextElementSibling.classList.add("text-danger");
-    name_input.nextElementSibling.innerText = `**Must be alphabetic characters.`;
+    name_input.nextElementSibling.innerText = `** Must be alphabetic characters.`;
     return;
   }
   if (name_input.value.length <= 3 || name_input.value.length >= 20) {
     e.preventDefault();
     name_input.nextElementSibling.classList.add("text-danger");
-    name_input.nextElementSibling.innerText = `**Must be between 3 to 20 long characters.`;
+    name_input.nextElementSibling.innerText = `** Must be between 3 to 20 long characters.`;
   } else {
     return;
   }
 };
-const password_validation = (pass,msg,e) => {
-  if (!pass.value) {
+const email_validation = (email_input, msg, e) => {
+  if (!email_input.value) {
     e.preventDefault();
-    pass.nextElementSibling.classList.add("text-danger");
-    pass.nextElementSibling.innerText = `**Please fill the ${msg} field.`;
+    email_input.nextElementSibling.classList.add("text-danger");
+    email_input.nextElementSibling.innerText = `** Please fill the ${msg} field.`;
     return;
   }
-  
-  if (pass.value.length <= 9 || pass.value.length >= 15) {
+  if (email_input.value.indexOf("@") <= 0) {
     e.preventDefault();
-    pass.nextElementSibling.classList.add("text-danger");
-    pass.nextElementSibling.innerText = `**Must be between 9 to 15 long numbers and alphabets.`;
+    email_input.nextElementSibling.classList.add("text-danger");
+    email_input.nextElementSibling.innerText = `** Invalid email address.`;
   } else {
     return;
   }
-}
+};
+const password_validation = (pass, msg, e) => {
+  if (!pass.value) {
+    e.preventDefault();
+    pass.nextElementSibling.classList.add("text-danger");
+    pass.nextElementSibling.innerText = `** Please fill the ${msg} field.`;
+    return;
+  }
+  if (pass.value.length <= 9 || pass.value.length >= 15) {
+    e.preventDefault();
+    pass.nextElementSibling.classList.add("text-danger");
+    pass.nextElementSibling.innerText = `** Must be between 9 to 15 long numbers and alphabets.`;
+  } else {
+    if (pass.value != confirmPassword.value){
+      e.preventDefault();
+      confirmPassword.nextElementSibling.classList.add("text-danger");
+      confirmPassword.nextElementSibling.innerText = "** Password mismatch. "
+    };
+  }
+};
+show_passwrod.addEventListener("click" , (e) => {
+  if(e.target.checked){
+    password.type = "text";
+  }else{
+    password.type = "password";
+  }
+});
 form.addEventListener("submit", (e) => {
   name_validation(firstName, "name", e);
   name_validation(userName, "username", e);
-  input_validation(email, "email", e);
-  password_validation(password, "password", e)
-  input_validation(confirmPassword, "password", e);
+  email_validation(email, "email", e);
+  password_validation(password, "password", e);
+  password_validation(confirmPassword, "password", e);
   input_validation(mobileNumber, "cell number", e);
 });
