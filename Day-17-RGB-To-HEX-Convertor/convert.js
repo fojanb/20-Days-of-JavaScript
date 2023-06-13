d80db30ee1a76ccf9b501977f4c9ef539a0c0672
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-06-13 14:45:49 -0700

    Convert hex to rgb done - now let's do the swap

--- Day-17-RGB-To-HEX-Convertor/convert.js
+++ Day-17-RGB-To-HEX-Convertor/convert.js
@@ -7,14 +7,18 @@ const blue = document.getElementById("blue-color");
 const rgb_background_color = document.getElementById("result-color");
 const result = document.getElementById("css-color");
 const convertBtn = document.getElementById("convert-button");
-convertBtn.addEventListener("click", () => {
-  // Step-1-Convert the user's input string to the hex mode
-  hex = userInput.value.toString(16);
-  // Step-2-Extract red, green and blue from hex variable
-  red.value = parseInt(hex.substring(0, 2), 16);
-  green.value = parseInt(hex.substring(2, 4), 16);
-  blue.value = parseInt(hex.substring(4, 6), 16);
-  result.value = `rgb(${red.value},${green.value},${blue.value})`;
-  rgb_background_color.style.backgroundColor = result.value;
+convertBtn.addEventListener("click", (e) => {
+  if (!userInput.value) {
+    e.preventDefault();
+  } else {
+    // Step-1-Convert the user's input string to the hex mode
+    hex = userInput.value.toString(16);
+    // Step-2-Extract red, green and blue from hex variable
+    red.value = parseInt(hex.substring(0, 2), 16);
+    green.value = parseInt(hex.substring(2, 4), 16);
+    blue.value = parseInt(hex.substring(4, 6), 16);
+    result.value = `rgb(${red.value},${green.value},${blue.value})`;
+    rgb_background_color.style.backgroundColor = result.value;
+  }
 });
 export { red, green, blue, result, rgb_background_color };
