90e7511c7dbf355e6597fa85ee7249deaadc3afa
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-10-30 16:49:28 -0700

    Refactored - 31 lines of code to 13 lines! yay

--- Day-11-Budget-App/app.js
+++ Day-11-Budget-App/app.js
@@ -1,31 +1,13 @@
-const data = {
-  budgetForm: document.querySelector("#budget-form"),
-  current_budget: document.getElementById("budget"),
-  expenseForm: document.getElementById("expense-form"),
-  current_expense: document.getElementById("expense"),
-  expense_title: document.getElementById("expense_title"),
-  current_balance : document.getElementById("balance"),
-  expenseValue : document.getElementById("expenseValue"),
-  budget:0,
-  expense:0,
-  balance:0,
+const showData = (arg) => {
+  document.getElementById(`_${arg}`).innerText = document.getElementById(
+    `${arg}`
+  ).value;
 };
-data.budgetForm.addEventListener("submit", (e) => {
+document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  data.budget = parseInt(data.budgetForm.querySelector("input").value);
-  data.current_budget.innerText = data.budget
-  data.current_balance.innerText = data.budget;
-  data.budgetForm.querySelector("input").value = "";
-  
-});
-data.expenseForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  data.expense = parseInt(data.expenseForm.querySelector("#expenseAmount").value);
-  data.current_expense.innerText = data.expense;
-  data.expenseValue.innerText = `$${data.expense}`;
-  data.expense_title.innerText = `-${document.getElementById("title").value}`;
-  data.balance = data.budget - data.expense
-  data.current_balance.innerText = data.balance;
-  document.getElementById("title").value="";
-  data.expenseForm.querySelector("#expenseAmount").value="";
+  showData("budget");
+  showData("expense");
+  document.getElementById("_balance").innerText =
+    document.getElementById("budget").value -
+    document.getElementById("expense").value;
 });
