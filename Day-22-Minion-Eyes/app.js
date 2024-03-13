a1ee1636d36296afdbba361032cc7d22597efb03
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-13 13:04:02 -0700

    Refactored the whole code to achieve a cleaner code (^_-)

--- Day-22-Minion-Eyes/app.js
+++ Day-22-Minion-Eyes/app.js
@@ -3,7 +3,8 @@ let threshold = {
     X: window.innerWidth / 2,
     Y: window.innerHeight / 2
 }
-const setEyePosition = (targetEye,pixels) => {
+let eyes = document.querySelectorAll(".eye-center");
+const setEyePosition = (targetEye, pixels) => {
     targetEye.style.right = `${pixels}px`;
     targetEye.style.top = "0px";
 }
@@ -13,18 +14,18 @@ const triggerEyes = (e) => {
     let clientYlessThanClientX = clientX > clientY;
     let topRegion = clientYlessThanClientX && clientYlessThanThresholdY;
     if (topRegion) {
-        clientX < threshold.X ? 
-        document.querySelectorAll(".eye-center").forEach(eye => {
-            if (pixelCounter !== 30) {
-                pixelCounter += 1;
-                setEyePosition(eye,pixelCounter);
-            }
-        }) : document.querySelectorAll(".eye-center").forEach(eye => {
-            if (pixelCounter >= 0) {
-                pixelCounter -= 1;
-                setEyePosition(eye,pixelCounter);
-            }
-        });
+        clientX < threshold.X ?
+            eyes.forEach(eye => {
+                if (pixelCounter !== 30) {
+                    pixelCounter += 1;
+                    setEyePosition(eye, pixelCounter);
+                }
+            }) : eyes.forEach(eye => {
+                if (pixelCounter >= 0) {
+                    pixelCounter -= 1;
+                    setEyePosition(eye, pixelCounter);
+                }
+            });
     }
 }
 window.addEventListener("mousemove", (e) => triggerEyes(e));
