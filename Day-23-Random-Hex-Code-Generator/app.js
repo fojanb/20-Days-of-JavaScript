5c54a0397856902a76b39bca1dcc40e63ccebb9b
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-18 12:46:04 -0700

    Done

--- Day-23-Random-Hex-Code-Generator/app.js
+++ Day-23-Random-Hex-Code-Generator/app.js
@@ -1,6 +1,5 @@
-const showHexCodeButton = document.querySelector("button");
-const hexCodeHolder = document.querySelector("#hexCode");
-
+const showRandomHexCode = document.querySelector("button");
+const randomHexCodeHolder = document.querySelector("#hexCode");
 const generateRandomHexCode = () => {
     let hexaDecimalNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
     let randomHexCode = [];
@@ -10,7 +9,9 @@ const generateRandomHexCode = () => {
         randomHexCode.push(hexaDecimalNumbers[randomIndex]);
         counter += 1;
     }
-    hexCodeHolder.innerHTML = `<span style="background-color:#${randomHexCode.join("")};color:white;">#${randomHexCode.join("")}</span>`;
+    randomHexCodeHolder.innerHTML = `
+    <span style="background-color:#${randomHexCode.join("")};color:white;">
+        #${randomHexCode.join("")}
+    </span>`;
 }
-
-showHexCodeButton.addEventListener("click", generateRandomHexCode);
+showRandomHexCode.addEventListener("click", generateRandomHexCode);
