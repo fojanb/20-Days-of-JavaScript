b2814e74b950c5f3f2be16634f3e69dc2bca435c
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-18 13:18:25 -0700

    Shorthand codes

--- Day-23-Random-Hex-Code-Generator/app.js
+++ Day-23-Random-Hex-Code-Generator/app.js
@@ -1,7 +1,7 @@
 const showRandomHexCode = document.querySelector("button");
 const randomHexCodeHolder = document.querySelector("#hexCode");
 const generateRandomHexCode = () => {
-    let randomHexCode = Math.floor(Math.random() * 11390625).toString(16);
+    let randomHexCode = Math.floor(Math.random() * 16777215).toString(16);
     randomHexCodeHolder.innerHTML = `
     <span style="background-color:#${randomHexCode};color:white;">
         #${randomHexCode}
