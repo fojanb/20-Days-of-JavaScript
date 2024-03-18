9287763c9d091400dddd7dc3c60b47d9bb8a631e
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-18 12:54:15 -0700

    Shorthand codes

--- Day-23-Random-Hex-Code-Generator/app.js
+++ Day-23-Random-Hex-Code-Generator/app.js
@@ -1,17 +1,10 @@
 const showRandomHexCode = document.querySelector("button");
 const randomHexCodeHolder = document.querySelector("#hexCode");
 const generateRandomHexCode = () => {
-    let hexaDecimalNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-    let randomHexCode = [];
-    let counter = 0;
-    while (counter < 6) {
-        let randomIndex = Math.floor(Math.random() * hexaDecimalNumbers.length);
-        randomHexCode.push(hexaDecimalNumbers[randomIndex]);
-        counter += 1;
-    }
+    let randomHexCode = Math.floor(Math.random() * 11390625).toString(16);
     randomHexCodeHolder.innerHTML = `
-    <span style="background-color:#${randomHexCode.join("")};color:white;">
-        #${randomHexCode.join("")}
+    <span style="background-color:#${randomHexCode};color:white;">
+        #${randomHexCode}
     </span>`;
 }
 showRandomHexCode.addEventListener("click", generateRandomHexCode);
