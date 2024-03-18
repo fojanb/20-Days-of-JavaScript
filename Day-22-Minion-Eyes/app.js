a471fa804cffe8a8bd5047897d91a88af18182c8
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-18 14:57:21 -0700

    Shorthand codes

--- Day-22-Minion-Eyes/app.js
+++ Day-22-Minion-Eyes/app.js
@@ -1,31 +1,45 @@
 let pixelCounter = 0;
-let threshold = {
-    X: window.innerWidth / 2,
-    Y: window.innerHeight / 2
-}
+let threshold = {X: window.innerWidth / 2, Y: window.innerHeight / 2}
 let eyes = document.querySelectorAll(".eye-center");
-const setEyePosition = (targetEye, pixels) => {
-    targetEye.style.right = `${pixels}px`;
-    targetEye.style.top = "0px";
+const setEyePosition = (targetEye, pixels, flag) => {
+    if (flag) {
+        targetEye.style.right = `${pixels}px`;
+        targetEye.style.top = "0px";
+    } else {
+        targetEye.style.bottom = "0px";
+        targetEye.style.top = `${pixels}px`;
+
+    }
 }
 const triggerEyes = (e) => {
     let { clientX, clientY } = e;
-    let clientYlessThanThresholdY = clientY < threshold.Y;
-    let clientYlessThanClientX = clientX > clientY;
-    let topRegion = clientYlessThanClientX && clientYlessThanThresholdY;
-    if (topRegion) {
+    let isTopRegion = clientY < threshold.Y;
+    if (isTopRegion) {
         clientX < threshold.X ?
             eyes.forEach(eye => {
                 if (pixelCounter !== 30) {
                     pixelCounter += 1;
-                    setEyePosition(eye, pixelCounter);
+                    setEyePosition(eye, pixelCounter, isTopRegion);
                 }
             }) : eyes.forEach(eye => {
                 if (pixelCounter >= 0) {
                     pixelCounter -= 1;
-                    setEyePosition(eye, pixelCounter);
+                    setEyePosition(eye, pixelCounter, isTopRegion);
                 }
             });
+    } else {
+      clientX > threshold.X ?
+        eyes.forEach(eye => {
+            if (pixelCounter !== 30) {
+                pixelCounter += 1;
+                setEyePosition(eye, pixelCounter, isTopRegion);
+            }
+        }) : eyes.forEach(eye => {
+            if (pixelCounter >= 0) {
+                pixelCounter -= 1;
+                setEyePosition(eye, pixelCounter, isTopRegion);
+            }
+        });
     }
 }
 window.addEventListener("mousemove", (e) => triggerEyes(e));
