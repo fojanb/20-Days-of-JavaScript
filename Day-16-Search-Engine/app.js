95dd679b4235341e98197703e0f46f2a2d9c10ff
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-21 16:32:36 -0700

    Refactored to achieve a better code performance

--- Day-16-Search-Engine/app.js
+++ Day-16-Search-Engine/app.js
@@ -1,41 +1,32 @@
-import { API_KEY_1, API_KEY_2 } from "./myapikeys.js";
 const searchInput = document.getElementById("search");
-const searchButton = document.querySelector("button");
+const form = document.querySelector("form");
 const holder = document.createElement("div");
 holder.classList.add("holder");
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": API_KEY_1,
-    "X-RapidAPI-Host": API_KEY_2,
-  },
+  headers: {Authorization: "Client-ID viAzAfoivZobXMdC197TdaT2qFhnKr2iLse4MnuuDPc"}
 };
 const makeGallery = (images) => {
-  images.forEach((image, index) => {
-    const frame = document.createElement("div");
-    const img = document.createElement("img");
-    const title = document.createElement("span");
-    title.innerText = image.title;
-    frame.classList.add("frame");
-    img.src = image.thumbnail;
-    img.alt = image.title;
-    img.setAttribute("id", index);
-    frame.append(img,title);
-    holder.append(frame);
-    document.body.append(holder)
-  });
+  if(!images){
+    alert("Ooppssie something went wrong");
+    return;
+  }
+  holder.innerText = images.map(image => {
+    `<div class="frame">
+      <img src=${image.alt_description} id=${image.id} alt=${image.alt_description} title=${image.alt_description}/>
+      <span>${image.alt_description}</span>
+    </div>`
+  }).join("");
+  console.log(holder)
+  document.body.append(holder)
 };
-const getImage = async () => {
+const getImage = async (e) => {
+  e.preventDefault();
   holder.innerHTML = null;
-  let data = await fetch(
-    `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=${searchInput.value}&pageNumber=1&pageSize=10&autoCorrect=true`,
-    options
-  ).then((response) => response.json());
-  console.log(data.value);
-  makeGallery(data.value);
+  const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchInput.value}`, options);
+  const data = await response.json();
+  makeGallery(data.results)
   searchInput.value="";
 };
 window.addEventListener("load", () => {
-  searchButton.addEventListener("click", getImage);
-
+  form.addEventListener("submit", getImage);
 });
