5674ee3069676fddd43b79337f2c88f81ebc789d
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-03-26 12:31:28 -0700

    Deleted extra white space inside the code.

--- Day-7-Addition-Game/app.js
+++ Day-7-Addition-Game/app.js
@@ -1,34 +1,34 @@
 const data = {
   num1: document.querySelector(".num1"),
   num2: document.querySelector(".num2"),
-  result: document.querySelector(".result"),
+  displayResult: document.querySelector(".result"),
   btn: document.querySelector(".btn"),
-  res: 0,
+  sum: 0,
 };
 const numberGenerator = () => {
   data.num1.innerText = Math.ceil(Math.random() * 10);
   data.num2.innerText = Math.ceil(Math.random() * 10);
 };
 const updateState = () => {
-  data.result.value = "";
+  data.displayResult.value = "";
   numberGenerator();
-  result(data.num1, data.num2);
+  add(data.num1, data.num2);
 };
 const checkAnswer = () => {
-  if (!data.result.value) {
+  if (!data.displayResult.value) {
     alert("You did not type any result yet...");
     return;
   }
-  if (data.res == parseInt(data.result.value)) {
+  if (data.sum == parseInt(data.displayResult.value)) {
     alert("Correct!");
     updateState();
     return;
   } 
-  alert(`Sorry, Incorrect. The correct answer was : ${data.res}`);
+  alert(`Sorry, Incorrect. The correct answer was : ${data.sum}`);
   updateState();
 }
-const result = (n1, n2) => {
-  data.res = parseInt(n1.innerText) + parseInt(n2.innerText);
+const add = (n1, n2) => {
+  data.sum = parseInt(n1.innerText) + parseInt(n2.innerText);
 };
 window.addEventListener("load", () => {
   updateState();
