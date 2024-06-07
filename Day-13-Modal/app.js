2c535ad7faf3c04aa225d43cdcc5fbb2ca4d208f
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-06-07 12:27:23 -0700

    Clean Code - Add function and get rid of extra messes

--- Day-13-Modal/app.js
+++ Day-13-Modal/app.js
@@ -2,12 +2,6 @@ const openModal = document.getElementById("openModal");
 const modal = document.querySelector(".modal");
 openModal.addEventListener("click", () => {
   modal.style.display = "block";
-  document.querySelector(".closeModal").addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-  window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-    }
-  });
+  document.querySelector(".closeModal").addEventListener("click", () => modal.style.display = "none");
+  window.addEventListener("click", (e) => e.target === modal ? modal.style.display = "none" : null);
 });
