da0535947ef9917e085a7b27ab3dc80f9fab4bf7
Fojan Babaali <Fojan.Babaali@ad.carmanah.com>
2024-06-07 12:16:53 -0700

    Clean Code - Add function and get rid of extra messes

--- Day-14-Tabs/vanilla_js/app.js
+++ Day-14-Tabs/vanilla_js/app.js
@@ -1,18 +1,20 @@
 import { tabs_info } from "./tab_infos.js";
 const article = document.querySelector("article");
 const tabs = document.querySelectorAll(".nav-bar .nav-item");
+const deactive_other_tabs = () => document.querySelectorAll(".active_tab").forEach(tab => tab.classList.remove("active_tab"));
+const initialState = () => {
+  document.getElementById("home").classList.add("active_tab");
+  article.innerHTML = `<p>home</p>`;
+}
 const handler = (e) => {
   let target = tabs_info.find(tab => tab.id === e.target.id);
-  document.querySelectorAll(".active_tab").forEach(tab => {
-    tab.classList.remove("active_tab");
-  });
+  deactive_other_tabs(); 
   e.target.classList.add("active_tab");
   article.innerHTML = `<p>${target.content}</p>`;
 };
-const tabState = () => tabs.forEach(tab => tab.addEventListener("click", (e) => handler(e)));
+const tabsState = () => tabs.forEach(tab => tab.addEventListener("click", (e) => handler(e)));
 
 window.addEventListener("load", () => {
-  tabState();
-  document.getElementById("home").classList.add("active_tab");
-  article.innerHTML = `<p>home</p>`;
+  initialState();
+  tabsState();
 });
